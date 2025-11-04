fe0b5d32719300534ebaa7dbee8b43cf9e6b91c1
roz-pnv <rozhinpanaw@gmail.com>
2025-11-04 15:00:45 +0330

    chore(auth): scaffold Sanctum infrastructure
    Install laravel/sanctum via install:api command.

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return response()->json(['message' => 'API is running']);
-});
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
